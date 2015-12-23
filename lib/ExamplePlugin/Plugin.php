8a21acd640f53f0da059e5bacfb4b33d62fb2192
Elliot Wright <wright.elliot@gmail.com>
2015-12-23 12:39:13 +0000

    Fixed some formatting issues, mixed tabs and spaces.

--- lib/ExamplePlugin/Plugin.php
+++ lib/ExamplePlugin/Plugin.php
@@ -1,22 +1,19 @@
 <?php
 
+class ExamplePlugin_Plugin extends Pimcore_API_Plugin_Abstract implements Pimcore_API_Plugin_Interface {
 
-class ExamplePlugin_Plugin  extends Pimcore_API_Plugin_Abstract implements Pimcore_API_Plugin_Interface {
-    
-	public static function install (){
+    public static function install (){
         // implement your own logic here
         return true;
-	}
-	
-	public static function uninstall (){
+    }
+
+    public static function uninstall (){
         // implement your own logic here
         return true;
-	}
+    }
 
-	public static function isInstalled () {
+    public static function isInstalled () {
         // implement your own logic here
         return true;
-	}
-
-
+    }
 }
